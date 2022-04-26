7906dad97e5c634ce2c58e58d9f75eecf5244a7b
Craig <indigomx9@gmail.com>
2022-04-26 17:11:52 -0400

    Initial setup of the project.

---
+++ README.md
@@ -0,0 +1,5 @@
+# CRUD
+
+Initial setup of the project.  Created with TypeScript, React, Router 6, Redux-Toolkit, RTK Query.
+
+
---
+++ src/app/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+export const App = (): JSX.Element => {
+    return (
+        <React.Fragment>
+            <h1>App</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi quam velit, fugit maiores tempore praesentium, recusandae atque rerum omnis numquam laboriosam fugiat labore, delectus quo ipsam tenetur ratione placeat deserunt.</p>
+        </React.Fragment>
+    );
+};
+
+
+
